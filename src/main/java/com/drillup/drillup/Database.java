5d9b28cea2f0808e248da94a22721cc054d59e11
Paul Muchemi <palomuchemi@gmail.com>
2025-01-20 22:09:04 +0300

    search Form Added

--- src/main/java/com/drillup/drillup/Database.java
+++ src/main/java/com/drillup/drillup/Database.java
@@ -1,4 +1,27 @@
 package com.drillup.drillup;
 
+import java.sql.*;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
 public class Database {
+
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet rs;
+
+
+    public Database() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection("jdbc:sqlserver://INGENUITYPAUL:1433;databaseName=KFHDAT;encrypt=false;trustServerCertificate=true", "sa", "Mahavira");
+            stmt = conn.createStatement();
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLServerDriver not found.");
+            //e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Connection failed."+ e.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/com/drillup/drillup/MainController.java
+++ src/main/java/com/drillup/drillup/MainController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
@@ -96,6 +97,7 @@ public class MainController {
 
     @FXML
     void initialize() {
+        Database db = new Database();
 
     }
 }
