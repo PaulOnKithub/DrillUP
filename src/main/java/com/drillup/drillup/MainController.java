06c6cf6eb9b8e3284ee50dc2a8caa37eb22c0c77
Paul Muchemi <palomuchemi@gmail.com>
2025-09-15 19:29:43 +0300

    Added excel cell formatting for date 2

--- src/main/java/com/drillup/drillup/MainController.java
+++ src/main/java/com/drillup/drillup/MainController.java
@@ -223,8 +223,8 @@ public class MainController {
                     row.createCell(2).setCellValue(arInfo2[0]);
                     //row.createCell(3).setCellValue(LocalDate.parse(arInfo2[1], customFormatter));
                     //row.createCell(4).setCellValue(LocalDate.parse(arInfo2[2],customFormatter));
-                    row.createCell(3,CellType.STRING).setCellValue(LocalDate.parse(arInfo2[1],customFormatter));
-                    row.createCell(4,CellType.STRING).setCellValue(LocalDate.parse(arInfo2[2],customFormatter));
+                    row.createCell(3,CellType.STRING).setCellValue(LocalDate.parse(arInfo2[1],customFormatter).toString());
+                    row.createCell(4,CellType.STRING).setCellValue(LocalDate.parse(arInfo2[2],customFormatter).toString());
                     row.createCell(5).setCellValue(arInfo2[3]);
                     row.createCell(6).setCellValue(arInfo2[4]);
                     row.createCell(7).setCellValue(arInfo2[5]);
